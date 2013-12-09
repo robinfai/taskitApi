28923d2b848613e91998992284111a347ef7b557
robinfai <robinfai9@gmail.com>
2013-12-09 23:01:59 +0800

    修复用户注册与登陆的测试用例

--- app/tests/UserTest.php
+++ app/tests/UserTest.php
@@ -6,6 +6,7 @@ class UserTest extends TestCase {
 
     public function setUp() {
         parent::setUp();
+        User::boot();
         if (!self::$isTruncate) {
             self::$isTruncate = true;
             DB::table('users')->delete();
@@ -15,7 +16,7 @@ class UserTest extends TestCase {
     /**
      * @dataProvider UserDataProvider
      */
-    public function testCreateUser($username, $password, $email) {
+    public function testRegister($username, $password, $email) {
         $userData = array('username' => $username, 'password' => $password, 'email' => $email);
         $this->client->request('POST', '/register', $userData);
         $id = $this->client->getResponse()->getContent();
@@ -25,10 +26,23 @@ class UserTest extends TestCase {
         $this->assertTrue($responseData['username'] == $userData['username']);
     }
 
+    /**
+     * @dataProvider UserDataProvider
+     */
+    public function testLogin($username, $password, $email) {
+        $userData = array('username' => $username, 'password' => $password, 'email' => $email);
+        $this->client->request('POST', '/login', $userData);
+        $id = $this->client->getResponse()->getContent();
+        $this->client->request('GET', '/user/' . $id);
+        $responseData = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertTrue($responseData['username'] == $userData['username']);
+    }
+
     public function UserDataProvider() {
         $data = array();
-        for ($i = 0; $i < 1; $i++) {
-            $username = 'user' . $i;
+        for ($i = 0; $i < 10; $i++) {
+            $username = 'username' . $i;
             $data[] = array('username' => $username, 'password' => $username, 'email' => $username . '@robinfai.com');
         }
         return $data;
