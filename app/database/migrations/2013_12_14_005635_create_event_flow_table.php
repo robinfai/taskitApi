45610cf618317e1db811769fb6d776d0929fb8b2
robinfai <robinfai9@gmail.com>
2013-12-14 09:08:51 +0800

    52aa4ef9092d05f76a0004ad 添加事件流数据表与模型类

---
+++ app/database/migrations/2013_12_14_005635_create_event_flow_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventFlowTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('event_flow', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('board_id');
+            $table->integer('user_id');
+            $table->string('event',1023);
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('event_flow');
+	}
+
+}
---
+++ app/models/EventFlow.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author RobinFai <robinfai9@gmail.com>
+ * @link http://bootstrap.robinfai.com
+ * @license http://www.yiiframework.com/license/
+ * @version $Id: EventFlow.php 2013-12-14 09:04 robin.fai $
+ */
+
+/**
+ * Class EventFlow
+ * 事件流
+ * @property int $id
+ * @property int $board_id
+ * @property int $user_id
+ * @property string $event
+ */
+class EventFlow extends Model{
+
+} 
